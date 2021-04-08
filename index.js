21a303f6756107425b38e268718e40416d0f3471
Isaac <izaroot327@gmail.com>
2021-04-08 13:43:17 -0400

    commit tree 87c9953cbf557fc525755961bd6160c593efbca7

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let newText = document.querySelector("#text")
+    newText.innerHTML = "This is really cool!"
+  }); 
